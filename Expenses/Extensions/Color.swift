6412215a6d3bce41ac291924b412d85cdc5cabe6
Juan Pablo <pankecho@JOKASTA.local>
2022-02-17 13:09:37 -0600

    Add .gitignore

---
+++ Expenses/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Expenses
+//
+//  Created by Juan Pablo Martinez Ruiz on 17/02/22.
+//
+
+import Foundation
---
+++ Expenses/Landing/LandingView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingView.swift
+//  Expenses
+//
+//  Created by Juan Pablo Martinez Ruiz on 17/02/22.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
